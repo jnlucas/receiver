41d76059877fd208e2effbc03b78edee8d0ce52f
logonrm <logonrm@fiap.com.br>
2018-02-17 10:34:40 -0200

    usando broadcast usb e boot

--- app/src/main/java/com/example/logonrm/demoordererbroadcast/BootBroadcastReceiver.kt
+++ app/src/main/java/com/example/logonrm/demoordererbroadcast/BootBroadcastReceiver.kt
@@ -3,13 +3,18 @@ package com.example.logonrm.demoordererbroadcast
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 /**
  * Created by logonrm on 17/02/2018.
  */
 class BootBroadcastReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
+        Toast.makeText(context,"terminei a inicializacao",Toast.LENGTH_LONG).show()
+
+
+
     }
 
 
--- app/src/main/java/com/example/logonrm/demoordererbroadcast/MainActivity.kt
+++ app/src/main/java/com/example/logonrm/demoordererbroadcast/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.logonrm.demoordererbroadcast
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var btEnviar: Button
+    lateinit var usbReceiver: BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +23,23 @@ class MainActivity : AppCompatActivity() {
         btEnviar.setOnClickListener({
             sendBroadcast(Intent("MeuOrderedBroadcast"))
         })
+
+
+        var filter = IntentFilter()
+        filter.addAction(Intent.ACTION_POWER_CONNECTED)
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+        val usbReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                if(intent?.action.equals(Intent.ACTION_POWER_CONNECTED)){
+                    tvUsbStatus.text = "cabo conectado"
+                }else{
+                    tvUsbStatus.text = "cabo desconectado"
+                }
+            }
+        }
+        registerReceiver(usbReceiver,filter);
+
+
     }
 }
